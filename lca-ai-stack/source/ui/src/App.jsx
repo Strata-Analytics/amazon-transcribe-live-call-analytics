94640968286c1585cbe4a05402f47c2039b5f62c
Dacaramo <dankramirez@outlook.com>
2026-05-22 15:37:08 -0500

    new deploy

--- lca-ai-stack/source/ui/src/App.jsx
+++ lca-ai-stack/source/ui/src/App.jsx
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+// New deploy
 import React, { useState } from 'react';
 import { Amplify, Logger } from 'aws-amplify';
 import { HashRouter } from 'react-router-dom';
